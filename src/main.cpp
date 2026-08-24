71644881504ded61549260d582713a9645c50833
Juan Naranjo <juanfnaran@gmail.com>
2026-08-24 17:13:54 -0400

    Add main.cpp and minesweeper.cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SFML/Graphics.h>
+#include <SFML/Audio.h>
+
+
+int main(int argc, char *argv[]) {
+     sf::RenderWindow window(sf::VideoMode(800, 600), "MineSweeper");
+    printf("Hello, MineSweeper!\n");
+    return 0;
+}
+
+void draw(sf::RenderWindow &window) {
+      sf::RenderWindow window(sf::VideoMode(800, 600), "MineSweeper");
+     
+}
+
+
+void loop();
